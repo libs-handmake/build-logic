315f53d466f3da70ebc61701144dd04960489197
Hoang <hoangdv@wavez.vn>
2024-08-20 14:25:49 +0700

    category mode config gradle

--- convention/src/main/kotlin/ApplicationPlugin.kt
+++ convention/src/main/kotlin/ApplicationPlugin.kt
@@ -1,12 +1,10 @@
 import com.android.build.api.dsl.ApplicationExtension
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 import plugin.ApplicationPlugin
 import util.android.defaultConfig
-import util.dependencies.CoreImplementation
+import java.time.Duration
+import java.time.Instant
 
 class ApplicationPlugin : ApplicationPlugin() {
     override fun Project.applyTarget(extension: ApplicationExtension, libs: VersionCatalog) {
@@ -14,5 +12,21 @@ class ApplicationPlugin : ApplicationPlugin() {
             apply("com.hoangdv.core")
         }
         extension.defaultConfig(this)
+
+        gradle.taskGraph.whenReady {
+            allTasks.forEach {
+                var time = Instant.now()
+                it.doFirst {
+                    time = Instant.now()
+                }
+                it.doLast {
+                    print(
+                        "\u001b[32mtask: ${it.name} took ${
+                            Duration.between(time, Instant.now()).toMillis()
+                        }ms\n\u001b[0m"
+                    )
+                }
+            }
+        }
     }
 }
